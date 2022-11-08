daefde1a10b40851b998dd1d7758353ddbfd4606
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-08 06:20:29 +0200

    added select

--- src/pages/Breeds.jsx
+++ src/pages/Breeds.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from '../components/Searchbar';
+import { requests } from '../servises/API';
 
 const Breeds = () => {
   const [query, setQuery] = useState('');
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await requests.getBreeds();
+        setBreeds(res.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    })();
+  }, []);
 
   const getQuery = (searchQuery) => {
     setQuery(searchQuery);
   };
-  console.log(query);
+
   return (
     <>
       <Searchbar getQuery={getQuery} />
+
+      <select>
+        <option disabled value="">
+          Breeds
+        </option>
+        {breeds.map((option) => (
+          <option key={option.id} value={option.name}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </>
   );
 };
---
+++ src/servises/API.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = `live_CqWfe3zoa8ucUnrhBYtcz5dvY7OOAPXZUD856Lf7C4SeVzy56bAO9ZrjP9ZeTA6C`;
+
+axios.defaults.baseURL = ` https://api.thecatapi.com/v1`;
+axios.defaults.headers.common['x-api-key'] = 'API_KEY';
+
+const endpoints = {
+  breeds: `/breeds`,
+};
+
+const getBreeds = async () => {
+  const res = await axios.get(endpoints.breeds);
+  return res;
+};
+
+export const requests = {
+  getBreeds,
+};

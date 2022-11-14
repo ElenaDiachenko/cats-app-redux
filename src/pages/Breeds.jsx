0958a7a9393cd97153ed1efcb3023c6c17711a34
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-14 23:06:19 +0200

    fixed slice

--- src/pages/Breeds.jsx
+++ src/pages/Breeds.jsx
@@ -1,39 +1,42 @@
 import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getAllBreeds } from '../redux/breed/breedsSlice';
+// import { useDispatch } from 'react-redux';
+// import { getAllBreeds } from '../redux/breed/breedsSlice';
+import { useGetBreedsQuery } from '../redux/breed/breedsApiSlice';
 import { Searchbar } from '../components/Searchbar';
 import { Select } from '../components/Select';
 import { requests } from '../servises/API';
 import { MasonryGallery } from '../components/MasonryGallery';
 
 const Breeds = () => {
-  const dispatch = useDispatch();
+  const { data, error, isLoading } = useGetBreedsQuery();
+  console.log(data);
+  // const dispatch = useDispatch();
   const [query, setQuery] = useState('');
-  const [breeds, setBreeds] = useState([]);
+  // const [breeds, setBreeds] = useState([]);
   const [limit, setLimit] = useState();
   const [shownPhotos, setShownPhotos] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await requests.getBreeds();
-        setBreeds(res.data);
-        dispatch(getAllBreeds(res.data));
-      } catch (error) {
-        console.log(error.message);
-      }
-    })();
-  }, [dispatch]);
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const res = await requests.getBreeds();
+  //       setBreeds(res.data);
+  //       // dispatch(getAllBreeds(res.data));
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   })();
+  // }, [dispatch]);
 
   useEffect(() => {
-    if (breeds.length === 0 && query === '') return;
+    if (!data && query === '') return;
     if (query === '') {
-      setShownPhotos(breeds);
+      setShownPhotos(data);
       return;
     }
-    const result = breeds.filter((breed) => breed.name.toLowerCase() === query);
+    const result = data.filter((breed) => breed.name.toLowerCase() === query);
     setShownPhotos(result);
-  }, [query, breeds]);
+  }, [query, data]);
 
   // const getBreedToShow = (query) => {
   //   if (breeds.length < 0) return;
@@ -71,27 +74,34 @@ const Breeds = () => {
 
   return (
     <>
-      <section className="md:flex md:items-center w-full gap-x-4">
-        <Searchbar getQuery={getInputQuery} />
-        <Select
-          defaultValue="All Breeds"
-          value={query}
-          options={breeds}
-          onChange={(value) => setQuery(value)}
-        />
-        <Select
-          value={limit}
-          onChange={(value) => setLimit(value)}
-          defaultValue="Limit"
-          options={[
-            { id: 5, name: '5' },
-            { id: 10, name: '10' },
-            { id: 15, name: '15' },
-            { id: 20, name: '20' },
-          ]}
-        />
-      </section>
-      {breeds.length > 0 && <MasonryGallery photos={shownPhotos} />}
+      {error && <p>Something went wrong</p>}
+      {isLoading && <p>Loading ...</p>}
+      {data && (
+        <>
+          <section className="md:flex md:items-center w-full gap-x-4">
+            <Searchbar getQuery={getInputQuery} />
+            <Select
+              defaultValue="All Breeds"
+              value={query}
+              options={data}
+              onChange={(value) => setQuery(value)}
+            />
+            <Select
+              value={limit}
+              onChange={(value) => setLimit(value)}
+              defaultValue="Limit"
+              options={[
+                { id: 5, name: '5' },
+                { id: 10, name: '10' },
+                { id: 15, name: '15' },
+                { id: 20, name: '20' },
+              ]}
+            />
+          </section>
+
+          <MasonryGallery photos={shownPhotos} />
+        </>
+      )}
       {/* {breedById.length > 0 && <MasonryGallery photos={breedById} />} */}
     </>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { themeReducer } from './theme/themeSlice';
-import { breedsReducer } from './breed/breedsSlice';
+// import { breedsReducer } from './breed/breedsSlice';
+import { breedsApi } from './breed/breedsApiSlice';
 
 export const store = configureStore({
   reducer: {
     darkTheme: themeReducer,
-    breeds: breedsReducer,
+    // breeds: breedsReducer,
+    [breedsApi.reducerPath]: breedsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    breedsApi.middleware,
+  ],
 });
+setupListeners(store.dispatch);

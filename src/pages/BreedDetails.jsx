0c7f3516b8c32de60610a10d22d7fafaeafaead5
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-15 00:15:01 +0200

    added Go Back

--- src/pages/BreedDetails.jsx
+++ src/pages/BreedDetails.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { allBreeds } from '../redux/breed/breedsSlice';
-import { useParams, useLocation } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { allBreeds } from '../redux/breed/breedsSlice';
+
+import { useParams, useLocation, Link } from 'react-router-dom';
 import { useGetBreedsQuery } from '../redux/breed/breedsApiSlice';
 import { requests } from '../servises/API';
 import { MdArrowBackIosNew } from 'react-icons/md';
 import Carousel from '../components/Carousel';
 const BreedDetails = () => {
-  // const breedsArray = useSelector(allBreeds);
   const { data, error, isLoading } = useGetBreedsQuery();
-
+  const location = useLocation();
+  console.log(location);
   const { id } = useParams();
   const [breeds, setBreeds] = useState([]);
   const breedDescr = data.find((it) => it.id === id);
@@ -34,10 +35,13 @@ const BreedDetails = () => {
       {isLoading && <p>Loading ...</p>}
       {data && (
         <div className="flex flex-col gap-y-4">
-          <div className="flex gap-x-4 items-center">
-            <MdArrowBackIosNew className="font-bold" />
-            <p>Breed</p>
-          </div>
+          <Link to={location.state?.from ?? '/breeds'}>
+            <div className="flex gap-x-4 items-center">
+              <MdArrowBackIosNew className="font-bold" />
+
+              <p>Go Breed</p>
+            </div>
+          </Link>
 
           <div className="flex flex-col gap-y-3 md:flex-row md:justify-between md:gap-x-4">
             <div className=" f-full md:w-[70%] bg-slate-600 rounded p-2 md:p-3 flex justufy-center items-center">

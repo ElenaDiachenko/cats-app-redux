b60883ed4c7524a87514ebd1040e6f45c84883f8
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-15 22:27:44 +0200

    fixed style pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Pagination = ({ currentPage, limit, total, paginate }) => {
   const pageNumbers = [];
   const activeClassName =
-    'bg-red-500 p-3 rounded-full flex justify-center items-center';
+    'rounded-full flex justify-center items-center w-[40px] h-[40px] font-bold bg-red-500 text-white hover:bg-slate-600';
 
   for (let i = 1; i <= Math.ceil(total / limit); i++) {
     pageNumbers.push(i);
@@ -11,17 +11,17 @@ export const Pagination = ({ currentPage, limit, total, paginate }) => {
 
   return (
     <nav>
-      <ul className=" flex justify-center mt-3 gap-x-3">
+      <ul className="mt-5 flex justify-center items-center gap-x-3">
         {pageNumbers.map((number) => (
-          <li
-            key={number}
-            className={
-              currentPage === number
-                ? activeClassName
-                : 'p-3 rounded-full flex justify-center items-center'
-            }
-          >
-            <button onClick={() => paginate(number)} className="">
+          <li key={number}>
+            <button
+              onClick={() => paginate(number)}
+              className={
+                currentPage === number
+                  ? activeClassName
+                  : 'rounded-full flex justify-center items-center w-[40px] h-[40px] font-bold bg-slate-500 hover:bg-slate-600'
+              }
+            >
               {number}
             </button>
           </li>

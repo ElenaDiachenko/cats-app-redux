337f3daf858fbbdac5b9039e572d6327a68ae82e
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-11-19 06:24:43 +0200

    fixed Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,49 +56,62 @@ const Home = () => {
   //     label: item.name,
   //   })),
   // ];
+  const paginate = (pageNumber) => setPage(pageNumber);
 
   return (
     <>
       {isLoading && <p>Loading ...</p>}
       {breeds.length > 0 && selectOptions && (
-        <section className=" flex flex-col  gap-y-3 md:flex-row md:items-center md:justify-between md:gap-x-3 ">
-          <div className="flex justify-between  w-full gap-x-3">
-            <Select
-              options={breedOptions}
-              placeholder="All Breeds"
-              classNamePrefix="custom-select"
-              className="
+        <>
+          <section className=" flex flex-col  gap-y-3 md:flex-row md:items-center md:justify-between md:gap-x-3 ">
+            <div className="flex justify-between  w-full gap-x-3">
+              <Select
+                options={breedOptions}
+                placeholder="All Breeds"
+                classNamePrefix="custom-select"
+                className="
     focus:outline-0 w-full md:w-[50%] font-bold text-gray-900 dark:text-white bg-gray-50 border border-gray-300 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-100"
-              onChange={(option) => setBreed(option.value)}
-            />
-            <Select
-              options={selectOptions.type}
-              placeholder="Type"
-              classNamePrefix="custom-select"
-              className="
+                onChange={(option) => setBreed(option.value)}
+              />
+              <Select
+                options={selectOptions.type}
+                placeholder="Type"
+                classNamePrefix="custom-select"
+                className="
     focus:outline-0 w-full md:w-[50%] font-bold text-gray-900 dark:text-white bg-gray-50 border border-gray-300 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-100"
-              onChange={(option) => setType(option.value)}
-            />
-          </div>
-          <div className="flex justify-between w-full gap-x-3">
-            <Select
-              options={selectOptions.order}
-              placeholder="Order"
-              classNamePrefix="custom-select"
-              className="
+                onChange={(option) => setType(option.value)}
+              />
+            </div>
+            <div className="flex justify-between w-full gap-x-3">
+              <Select
+                options={selectOptions.order}
+                placeholder="Order"
+                classNamePrefix="custom-select"
+                className="
     focus:outline-0 w-full md:w-[50%] font-bold text-gray-900 dark:text-white bg-gray-50 border border-gray-300 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-100"
-              onChange={(option) => setOrder(option.value)}
-            />
-            <Select
-              options={selectOptions.limit}
-              placeholder="Limit"
-              classNamePrefix="custom-select"
-              className="
+                onChange={(option) => setOrder(option.value)}
+              />
+              <Select
+                options={selectOptions.limit}
+                placeholder="Limit"
+                classNamePrefix="custom-select"
+                className="
     focus:outline-0 w-full md:w-[50%] font-bold text-gray-900 dark:text-white bg-gray-50 border border-gray-300 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-100"
-              onChange={(option) => setLimit(option.value)}
+                onChange={(option) => setLimit(option.value)}
+              />
+            </div>
+          </section>
+          {currentPhotos && <MasonryGallery photos={currentPhotos} />}
+
+          {shownPhotos.length > 0 && (
+            <Pagination
+              limit={limit}
+              total={total}
+              paginate={paginate}
+              currentPage={page}
             />
-          </div>
-        </section>
+          )}
+        </>
       )}
     </>
   );
